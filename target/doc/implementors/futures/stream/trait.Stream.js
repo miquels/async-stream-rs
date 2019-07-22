d0fe4de81625f19b0c9d40ddce4c11f4b6c7a842
Miquel van Smoorenburg <mike@langeraar.net>
2019-07-23 00:06:16 +0200

    target/doc update

---
+++ target/doc/implementors/futures/stream/trait.Stream.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["async_stream"] = [{text:"impl&lt;I, E&gt; <a class=\"trait\" href=\"https://docs.rs/futures/0.1/futures/stream/trait.Stream.html\" title=\"trait futures::stream::Stream\">Stream</a> for <a class=\"struct\" href=\"async_stream/struct.AsyncTryStream.html\" title=\"struct async_stream::AsyncTryStream\">AsyncTryStream</a>&lt;I, E&gt;",synthetic:false,types:["async_stream::AsyncTryStream"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
